9103ce31b6d56a7e7a5672315b28c0a2cf5497af
mahavir vaishnav <mvaishnav888@gmail.com>
2021-11-10 15:40:38 +0530

    returned cors back as it as

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const cookieParser = require('cookie-parser');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: "*",
+}));
 app.use(cookieParser());
 
 dotenv.config({ path: "config/config.env" });
