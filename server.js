d2fcb8bb2fd318abac4239c2396bff2564b2116f
mahavir vaishnav <mvaishnav888@gmail.com>
2021-11-10 15:14:58 +0530

    second commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:3000"
+  origin: "https://martacart.herokuapp.com/"
 }));
 app.use(cookieParser());
 
