eec23f2fc1b0c11c37d5fedb1e298cd9f191bafe
mahavir vaishnav <mvaishnav888@gmail.com>
2021-11-10 15:16:29 +0530

    cors policy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: "https://martacart.herokuapp.com/"
+  origin: "https://martacart.herokuapp.com"
 }));
 app.use(cookieParser());
 
