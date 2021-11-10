ad1fab12c2ab08f9ebfafb08c32fbdd549f50882
mahavir vaishnav <mvaishnav888@gmail.com>
2021-11-10 22:14:47 +0530

    removed authentication

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {processPayment, sendAPIKey} = require('../controller/paymentController');
 const {isAuthenticatedUser} = require('../utils/isAuthenticated');
 
-router.route('/payment/process').post(isAuthenticatedUser, processPayment);
-router.route('/stripeapi').get(isAuthenticatedUser, sendAPIKey);
+router.route('/payment/process').post(processPayment);
+router.route('/stripeapi').get(sendAPIKey);
 
 module.exports = router;
